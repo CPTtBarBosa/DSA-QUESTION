080ba2fc0864242875ae2e8c5fef772fc5c1fc80
Rupam Singh <159822330+CPTtBarBosa@users.noreply.github.com>
2026-05-12 23:42:11 +0530

    Time: 26 ms (28.85%), Space: 114 MB (29.81%) - LeetHub

---
+++ 1665-minimum-initial-energy-to-finish-tasks/1665-minimum-initial-energy-to-finish-tasks.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int minimumEffort(int[][] tasks) {
+        Arrays.sort(tasks,(a, b) -> (b[1] - b[0]) - (a[1] - a[0]));
+        int currentEnergy=0;
+        int totalRequirement=0;
+        for (int[] task : tasks) {
+            int actual = task[0];
+            int minimum = task[1];
+            if (currentEnergy < minimum) {
+             
+                totalRequirement += (minimum - currentEnergy);
+
+                currentEnergy = minimum;
+            }
+            
+            
+            currentEnergy -= actual;
+        }
+
+        return totalRequirement;
+    }
+}
